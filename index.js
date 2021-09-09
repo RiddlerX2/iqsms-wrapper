2c1224ae7b157e760e5bce9bd3397ae94c019864
RiddlerX2 <timofeev.andreyx2@gmail.com>
2021-09-09 22:16:19 +0300

    1.0.1
    Initial release

---
+++ index.js
@@ -0,0 +1,141 @@
+/*
+	IQSMS wrapper v.1.0.x
+	Allow send SMS using IQSMS service
+	
+	For more information about operations and its parameters look at:
+		https://iqsms.ru/api/api_rest/
+*/
+
+/*Define dependencies*/
+const axios = require('axios');
+
+/*Error mesages*/
+const messages = {
+	ru : {
+		operation_error : 'Указанная операция не определена в документации банка',
+		params_invalid : 'Параметры должны быть переданы в виде массива',
+		callback_invalid : 'Требуется функция обратного вызова',
+		value_invalid : 'Параметр должен быть указан',
+		value_type_invalid : 'Некорректный тип данных ',
+		auth_invalid : 'Некорректные данные для авторизации, необходим либо токен либо имя пользователя и пароль. Для операции возврата имя пользователя и пароль обязательны.'
+	},
+	en : {
+		operation_error : 'The specified operation is not defined in the banks documentation',
+		params_invalid : 'Parameters must be passed as an array',
+		callback_invalid : 'Callback function required',
+		value_invalid : 'Value must be specified',
+		value_type_invalid : 'Value type is incorrect ',
+		auth_invalid : 'Incorrect authorization data token or userName and password must be provided. For refund operation userName and password required.'
+	}
+}
+
+const commands = [
+	'balance', 
+	'send',
+	'status'
+];
+
+/*Main class*/
+class IQSMS {
+	#language;
+	#username;
+	#password;
+	/*Default REST point of IQSMS*/
+	execURLPrefix = `https://api.iqsms.ru/messages/v2/`;
+	execURLSuffix = `.json`;
+	/*Initialise class with "new"*/
+	constructor (userName, password, language, execURLPrefix, execURLSuffix) {
+		this.#language = language;
+
+		if (!userName || !password) {
+			throw messages[this.#language].auth_invalid;
+		}
+
+		this.#username = userName;
+		this.#password = password;
+		/*If URLs not defines use default values*/
+		if (execURLPrefix) {
+			this.execURLPrefix = execURLPrefix;
+		};
+		if (execURLSuffix) {
+			this.execURLSuffix = execURLSuffix;
+		};
+	};
+	
+	/*Simple detectors of Objects and Arrays*/
+	isObject = (a) => {
+		return (!!a) && (a.constructor === Object);
+	};
+	
+	isArray = (a) => {
+    return (!!a) && (a.constructor === Array);
+	};
+	
+	/*Unified execute action with callback*/
+	execute(operation, data, callback) {
+		if (!callback instanceof Function) {
+			throw messages[this.#language].callback_invalid;
+		} else if (!this.isArray(data)) {
+			callback(messages[this.#language].params_invalid, false);
+		} else if (!commands.includes(operation)) {
+			callback(messages[this.#language].operation_error, false);
+		} else {
+			/*Extend parameters by adding authorization token*/
+			if (this.#username && this.#password) {
+				data.login = this.#username;
+				data.password = this.#password;
+			}
+			if (data.length) {
+				data.messages = data;
+			}
+			/*Send data to server*/
+			axios({
+				method : 'POST', //as described in documentation (link above)
+				url : `${this.execURLPrefix}${operation}${this.execURLSuffix}`,
+				data : data
+			}).then(
+				(result) => {
+					/*If operation succeeded on API's side return data*/
+					if (result.data.status == 'ok') {
+						callback(false, result.data);
+					} else {
+						/*Else return data with description as error*/
+						callback(result.data, false);
+					}
+				}
+			).catch(
+				(error) => {
+					callback(error, false);
+				}
+			)
+		}
+	}
+	/*Promisified execution for those who used a single thread application and uses await*/
+	executePromise(operation, data) {
+		return new Promise((resolve, reject) => {
+			this.execute(operation, data, (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
+
+	/*Functions for most popular actions*/
+	send(messages) {
+		return this.executePromise('send', messages);
+	}
+	
+	status(messages) {
+		return this.executePromise('status', messages);
+	}
+
+	balance() {
+		return this.executePromise('balance', []);
+	}
+}
+
+/*Export class to outside*/
+exports.IQSMS = IQSMS;
